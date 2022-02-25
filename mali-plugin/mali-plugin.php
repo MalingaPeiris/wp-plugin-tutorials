29739ba1b63e6a3c8884d323e9f637ad24a80d36
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-02-25 16:27:20 +0530

    part 3

--- mali-plugin/mali-plugin.php
+++ mali-plugin/mali-plugin.php
@@ -1,24 +1,44 @@
-<php
-/***
+<?php
+
+/**
 @package MaliPlugin
  */
 
- /**
+/**
  Plugin Name: Mali Plugin 
  Description: The first eveer WP plugin by Mr. Malinga
  Version: 1.0
  Author: Mr. Malinga
  License: GPLv2 or later
  Text Domain: mali-plugin
-  */
+ */
 
-  if(! defined('ABSPATH')){
-      die;
-  }
+defined('ABSPATH') or die('No Access!');
 
-  //defined ('ABSPATH') or die('No Access!');
+class MaliPlugin
+{
+    function activate()
+    {
+        //generate a CPT
+        //flush rewrite rules
+    }
 
-  /*** if(! function_exists('add_action')){
-      echo 'Access Denied!';
-      exit;
-  } ***/
+    function deactivate()
+    {
+        //flush rewrite rules
+
+    }
+
+    function unistall()
+    {
+        //delete CPT
+    }
+}
+if (class_exists('MaliPlugin')) {
+    $maliPlugin = new MaliPlugin();
+}
+//activation
+register_activation_hook(__FILE__, array($maliPlugin, 'activate'));
+
+//deactivate
+register_deactivation_hook(__FILE__, array($maliPlugin, 'deactivate'));
