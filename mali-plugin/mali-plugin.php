bf5928115e1a4e1d9c03a95d082289857170ea05
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-02-28 09:16:46 +0530

    custom post type

--- mali-plugin/mali-plugin.php
+++ mali-plugin/mali-plugin.php
@@ -6,7 +6,7 @@
 
 /**
  Plugin Name: Mali Plugin 
- Description: The first eveer WP plugin by Mr. Malinga
+ Description: The first ever WP plugin by Mr. Malinga
  Version: 1.0
  Author: Mr. Malinga
  License: GPLv2 or later
@@ -17,21 +17,32 @@ defined('ABSPATH') or die('No Access!');
 
 class MaliPlugin
 {
+    function __construct()
+    {
+        add_action('init', array($this, 'custom_post_type'));
+    }
     function activate()
     {
         //generate a CPT
+        $this->custom_post_type();
         //flush rewrite rules
+        flush_rewrite_rules();
     }
 
     function deactivate()
     {
         //flush rewrite rules
-
+        flush_rewrite_rules();
     }
 
     function unistall()
     {
         //delete CPT
+    }
+
+    function custom_post_type()
+    {
+        register_post_type('book', ['public' => true, 'label' => 'Books']);
     }
 }
 if (class_exists('MaliPlugin')) {
