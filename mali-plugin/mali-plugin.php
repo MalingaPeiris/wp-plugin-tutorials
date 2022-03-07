41a98ddb0594c8b608b56186c5b1375fed285ed5
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-03-07 14:58:20 +0530

    starter plugin structure

--- mali-plugin/mali-plugin.php
+++ mali-plugin/mali-plugin.php
@@ -21,6 +21,24 @@ if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
 
 define ('PLUGIN_PATH', plugin_dir_path(__FILE__));
 define ('PLUGIN_URL', plugin_dir_url(__FILE__));
+define('PLUGIN', plugin_basename(__FILE__));
+
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+
+function activate_mali_plugin()
+{
+    Activate::activate();
+}
+
+function deactivate_mali_plugin()
+{
+    Deactivate::deactivate();
+}
+
+register_activation_hook(__FILE__, 'activate_mali_plugin');
+register_deactivation_hook(__FILE__, 'deactivate_mali_plugin');
+
 
 if(class_exists('Inc\\Init')){
     Inc\Init::register_services();
