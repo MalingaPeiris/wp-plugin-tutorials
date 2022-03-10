27f119b0f476a6052c826c7e49673e0375e9c94b
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-03-10 16:35:21 +0530

    admin tabs in vanilla JS

--- mali-plugin/inc/Base/enqueue.php
+++ mali-plugin/inc/Base/enqueue.php
@@ -18,7 +18,7 @@ class Enqueue extends BaseController
     function enqueue()
     {
         //enqueue all scripts
-        wp_enqueue_style('mypluginstyle',$this->plugin . 'assets/mystyle.css');
-        wp_enqueue_script('mypluginscript', $this->plugin . 'assets/myscript.js');
+        wp_enqueue_style('mypluginstyle', $this->plugin_url . 'assets/mystyle.css');
+        wp_enqueue_script('mypluginscript', $this->plugin_url . 'assets/myscript.js');
     }
 }
--- mali-plugin/templates/admin.php
+++ mali-plugin/templates/admin.php
@@ -2,6 +2,14 @@
     <h1>Mali Plugin Required</h1>
     <?php settings_errors() ?>
 
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#tab-1">Manage Settings</a></li>
+        <li><a href="#tab-2">Updates</a></li>
+        <li><a href="#tab-3">About</a></li>
+    </ul>
+
+    <div class="tab-content">
+        <div id="tab-1" class="tab-pane active">
     <form method="post" action="options.php">
         <?php
         settings_fields('mali_options_group');
@@ -9,4 +17,13 @@
         submit_button();
         ?>
     </form>
+    </div>
+
+    <div class="tab-pane" id="tab-2">
+        <h3>Updates</h3>
+    </div>
+    <div class="tab-pane" id="tab-3">
+        <h3>About</h3>
+    </div>
+</div>
 </div>
